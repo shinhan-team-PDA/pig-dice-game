afd40e818665a384aba69f2ceb351dbc1317a1c3
kji-dec <dec22sag@gmail.com>
2023-01-13 14:01:23 +0900

    feat: Create player, computer, user classes
    created classes player, computer, user for OOP.
    defined bank function into parent class Player.

--- pigdice.py
+++ pigdice.py
@@ -1,20 +1,31 @@
 from random import randint
 
 # class computer
+class Player:
+    def __init__(self):
+        self.score = 0
+        self.bank = 0
+
+    def bank(self):
+        self.score += self.bank
+        return self.score
+
+
+class Computer(Player):
+    def __init__(self):
+        Player.__init__(self)
+
 
 # class user
+class User(Player):
+    def __init__(self):
+        Player.__init__(self)
 
 # 사용자 정보 받기
+
 
 # Roll 기능 구현
 
 # Bank 기능 구현
 
 # Roll stop
-
-
-
-
-
-
-
