905fddbd2f77d97512250008357bddda08502dce
kji-dec <dec22sag@gmail.com>
2023-01-13 14:14:02 +0900

    feat: Create input_user function
    get user input to initiate user and computer.

--- pigdice.py
+++ pigdice.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 # class computer
 class Player:
     def __init__(self):
@@ -18,11 +19,17 @@ class Computer(Player):
 
 # class user
 class User(Player):
-    def __init__(self):
+    def __init__(self, name):
         Player.__init__(self)
+        self.user_name = name
+
 
 # 사용자 정보 받기
-
+def input_user():
+    print("============================")
+    print("welcome to pig dice game!")
+    user_name = input("Please Enter Your NAME: ")
+    return User(user_name), Computer()
 
 # Roll 기능 구현
 
